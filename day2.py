cc42d41a3f940e1c849a92668b3956cb8ac46527
nrsundberg <nrsundbe@ncsu.edu>
2022-12-02 13:25:44 -0500

    del object not needed

--- day2.py
+++ day2.py
@@ -11,15 +11,12 @@ for line in lines:
 totalScore
 
 # Part 2
-identify = {
-    'X': 0, 'Y': 1, 'Z': 2
-}
 different = {
-    0: {'A':3,'B':1,'C':2},
-    1: {'A':4,'B':5,'C':6},
-    2: {'A':8,'B':9,'C':7}
+    'X': {'A':3,'B':1,'C':2},
+    'Y': {'A':4,'B':5,'C':6},
+    'Z': {'A':8,'B':9,'C':7}
 }
 totalScore = 0
 for line in lines:
-    totalScore += different[identify[line[2]]][line[0]]
+    totalScore += different[line[2]][line[0]]
 totalScore
